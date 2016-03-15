85144b0b0f1bcd3253a319b8c2e95773e8e775b9
Farhad Haqiqat <haqiqath@ualberta.ca>
2016-03-15 13:03:01 -0600

    not tested PCA

--- PCA.m
+++ PCA.m
@@ -1,15 +1,15 @@
-function [train_X,train_Y,test_X,test_Y]=PCA(internal)
+function [train_X,test_X_pca]=PCA(internal)
 [train_X,train_Y,test_X,test_Y] = generateBalancedDataSet(feature,k);
 
 if internal==0,
     prompt = 'Please enter number of Principal component that you want to have(less than 304): ';
     principal = input(prompt);
-    X_Train_pca= ppca(train_X,principal);
-    X_Test_pca = ppca(X_Test,principal);
+    train_X_pca= ppca(train_X,principal);
+    test_X_pca = ppca(X_Test,principal);
 else 
     
-    X_Train_pca = getPCA(train_X,k);
-    X_Test_pca = getPCA(test_X,k);
+    train_X_pca = getPCA(train_X,k);
+    test_X_pca = getPCA(test_X,k);
 end
 end
 function Z=getPCA(X,k)
