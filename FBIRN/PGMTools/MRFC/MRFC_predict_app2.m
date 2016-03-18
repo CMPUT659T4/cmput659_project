ff0882f8a8ad438e0ad73cd89f7d6e3e21fcfac0
shashi3876 <shashindra3@gmail.com>
2016-03-17 21:03:04 -0600

    Sum changes

---
+++ FBIRN/PGMTools/MRFC/MRFC_predict_app2.m
@@ -0,0 +1,35 @@
+function [y,pyx] = MRFC_predict(X, model)
+% Use Gaussian Markov Random Field classification model (class-conditional
+% models P(X|C) and class prior P(C)) to predict class labels of a given
+% set of training instances X
+% model = struct('nvars',..,'nlabels',.., 'class_cond',..,'class_prior',..);  
+
+[n,p]=size(X);
+p=model.nvars; % number of variables/dimensions
+k=model.nlabels; % number of class labels
+py=model.class_prior;
+cond = model.class_cond; % k-dimensional array of structs with fields C and mu
+
+% select max-likelihood label:
+% max_i P(Y=i|X=x) = max_i P(X=x,Y=i) label since P(Y=i|X) = P(X=x,Y=i)/P(X=x)
+CC = inv(cond(1).C);
+    CC = (CC + CC')/2;
+    cond(1).mu;
+   mvnpdf(X,cond(1).mu, CC );
+   X;
+pyx = zeros(n,k);
+del=0.01;
+% compute P(X,Y=i)=P(Y=i)*P(X=x|Y=i) for each class lable i
+for class=1:k
+    CC = inv(cond(class).C);
+    CC = (CC + CC')/2;
+     pyx(:,class) = log(py(class))*(mvncdf(X+del,cond(class).mu, CC )-mvncdf(X-del,cond(class).mu, CC ));
+    %pyx(:,class) = log(py(class))+logmvnpdf(X,cond(class).mu, CC ); % multivar gaussian pdf
+end
+
+pyx;
+[a,ind]=max(pyx');
+y=zeros(n,1);
+for i=1:n
+    y(i)=model.labels(ind(i));
+end
--- calcRishProjGrad.m
+++ calcRishProjGrad.m
@@ -5,6 +5,7 @@ addpath('FBIRN/PGMTools/SparseMRF','-end');
 addpath('FBIRN/PGMTools/MRFC','-end');
 addpath('FBIRN/PGMTools/SparseMRF/','-end');
 addpath(genpath('FBIRN/PGMTools/SparseMRF/'))
+addpath(genpath('FBIRN/PGMTools/MRFC/'))
 %addpath('FBIRN/PGMTools/SparseMRF','-end')
 
 site2=site;
@@ -15,16 +16,17 @@ for i=1:itterat
     %%% generating the test and the training set   
    
    [X_train,Y_train,X_test,Y_test]=generateBalancedDataSet(6,5,3);
+   size(X_test);
     X_train=X_train(:,1:limit);
      X_test=X_test(:,1:limit);  
     lambdas=[0 0.01 0.1 0.5 1 5];
     %lambdas=[0.7];% 0.5 1 5];
-    %lambdas=[0.1:0.2:0.9];;% 0.5 1 5];
+    %lambdas=[0.5:0.2:0.9];;% 0.5 1 5];
     lambdas=0.7;
    % method='projected_gradient';
     method='varsel_mrf';
     %method='sinco';
-    
+    %method='our_covsel';
     
     
     %          model = MRFC_CVlambda_learn(X_train, Y_train, method, lambda);  %learn MRF classifier (MRFC)
